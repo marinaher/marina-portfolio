d1d4d01c16b88c096e57b14c3282a76a40d5dc73
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-05-26 15:41:51 -0500

    adjusted create-react-app as needed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
 registerServiceWorker();
