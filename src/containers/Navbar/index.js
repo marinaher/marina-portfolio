ff6cc2a49f87ffcc302ae8dfe3e31041a27ca529
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-06-01 15:48:34 -0500

    Added a logo

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -5,6 +5,7 @@ import Contact from '../../components/Contact';
 import Education from '../../components/Education';
 import Employment from '../../components/Employment';
 import Home from '../../components/Home';
+import MyLogo from '../../images/myLogo.png';
 import Portfolio from '../../components/Portfolio';
 import Skills from '../../components/Skills';
 import './navbar.css';
@@ -13,13 +14,13 @@ const Navbar = () => (
   <Router>
     <nav className="sidebar sidebar-block">     
       <ul className="list">
-        <li className="list-item"><Link to="/">Add <br /> Logo <br /> Here</Link></li>
-        <li className="list-item"><Link to="/AboutMe">About Me</Link></li>
-        <li className="list-item"><Link to="/Skills">Skills</Link></li>
-        <li className="list-item"><Link to="/Education">Education</Link></li>
-        <li className="list-item"><Link to="/Employment">Employment</Link></li>
-        <li className="list-item"><Link to="/Portfolio">Portfolio</Link></li>
-        <li className="list-item"><Link to="/Contact">Contact</Link></li>
+        <li className="list-item"><Link to="/"><img className="logo" src={MyLogo} alt="Logo"/></Link></li>
+        <li className="list-item"><Link to="/AboutMe">ABOUT ME</Link></li>
+        <li className="list-item"><Link to="/Skills">SKILLS</Link></li>
+        <li className="list-item"><Link to="/Education">EDUCATION</Link></li>
+        <li className="list-item"><Link to="/Employment">EMPLOYMENT</Link></li>
+        <li className="list-item"><Link to="/Portfolio">PORTFOLIO</Link></li>
+        <li className="list-item"><Link to="/Contact">CONTACT</Link></li>
       </ul>
 
       <Route exact={true} path="/" component={Home} />
