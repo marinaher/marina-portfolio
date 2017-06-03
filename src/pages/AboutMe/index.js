c096dc7f9a9492b8d8538dff48ef950e1c8cc4a4
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-06-03 11:39:25 -0500

    changed pageTitle from About Me to About

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Content from '../../components/Content';
 
 const AboutMe = () => (
-  <Content pageId='aboutme' pageTitle='About Me' link='About'>
+  <Content pageId='aboutme' pageTitle='About' link='About'>
     <p>Lorem ipsum dolor sit amet, et tantas labitur nec. Tempor accusamus vix cu, at oblique gloriatur mel, mea id lucilius consequat. Ei tritani civibus sed, suscipit evertitur vis et, ullum utamur cu his. In justo omittantur ius.</p>
 
     <div className="myInfo">
