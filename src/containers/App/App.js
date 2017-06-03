59a2eea6a577ff802a16702b85fe7ab74501538b
Marina Her <Marina@Marinas-MacBook-Pro.local>
2017-06-03 15:06:07 -0500

    added space in between closing tag

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <NavList/>
+        <NavList />
         <Home />
         <AboutMe />
         <Skills />
